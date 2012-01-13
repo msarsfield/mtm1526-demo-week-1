83f164c7a85004a3a1b5e83ee9c919abf26819e4
msarsfield <msarsfield@rogers.com>
2012-01-13 11:37:01 -0500

    Name and name returned

--- js/java.js
+++ js/java.js
@@ -6,4 +6,18 @@ Javascript multin-line comments
 // JavaScript Document
 */
 
-alert('Bring your shovel...');
+alert('This is an alert.');
+
+// prompt() is for user input, has a text field for the user to enter some text
+
+var name = prompt('What is your name?'); // if you want to collect the name, you have to create a variable as we do in action script
+
+// in javascipt, a trqce is called console.log.  It writes out the content of a variable for debugging
+
+console.log(name); //to see the trace you have to go to firebug
+
+//document where all the html is located
+
+document.write('Hello, '+ name);
+
+// window which allows you to get at the URL bar the backbutton
